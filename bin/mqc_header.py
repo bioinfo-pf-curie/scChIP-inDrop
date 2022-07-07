d160ba3b535be78db446002e8566a06278559a10
LouisaHadj <louisa.hadj-abed@curie.fr>
2022-07-07 19:27:00 +0200

    test line in coverage mqc

--- bin/mqc_header.py
+++ bin/mqc_header.py
@@ -89,10 +89,10 @@ if args.nbreads > 0:
 ##
 
 multiqc_list += ["custom_plot_config:"]
-multiqc_list += ["   umiDistrib_plot:"]
+multiqc_list += ["   umiDistrib:"]
 multiqc_list += ["      xPlotLines:"]
 multiqc_list += ["         - color: '#a9a9a9'"]
-multiqc_list += ["           value: 2 "]
+multiqc_list += ["           value: " + str(2)]
 multiqc_list += ["           dashStyle: 'LongDash'"]
 multiqc_list += ["           width: 1"]
 multiqc_list += ["           label:"]
